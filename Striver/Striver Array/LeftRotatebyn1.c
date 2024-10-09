75d30b825ca0d987f4eac2756407afc15012394f
Raj Madgulkar <rajmadgulkar1007@gmail.com>
2024-10-09 21:06:15 +0530

    Initial Commit

---
+++ Striver/Striver Array/LeftRotatebyn1.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void swap(int *a,int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+void reverse(int *arr,int left,int right){
+    right =right-1;
+    while(left<right){
+        swap(&arr[left],&arr[right]);
+        left++;
+        right--;
+    }  
+}
+void leftRotatebyn(int *arr,int size,int d){
+
+    d=d%size;
+
+    reverse(arr,0,d);
+    reverse(arr,d,size);
+    reverse(arr,0,size);
+    
+}
+int main(){
+    int arr[]={1,2,3,4,5,6,7};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int d=18;
+
+    leftRotatebyn(arr,size,d);
+
+    for(int i=0;i<size;i++){
+        printf(" %d",arr[i]);
+    }
+
+    return 0;
+}
